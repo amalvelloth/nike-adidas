70e45ff31a8d45e1f3999d060e360b678d92f372
amalvelloth <amalv414@gmail.com>
2024-05-07 13:26:48 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import nikeBGimage from "../assets/nike.jpg";
-import adidasBGimage from "../assets/adidas.jpg";
+import React from "react";
+import { useState } from "react";
 import nike from "../assets/nike.svg";
 import adidas from "../assets/adidas.svg";
+import nikeBgImage from "../assets/nike.jpg";
+import adidasBgImage from "../assets/adidas.jpg";
 import LoadingBar from "react-top-loading-bar";
 
 function Hero() {
@@ -11,7 +12,7 @@ function Hero() {
   const [isAdidasExpanded, setIsAdidasExpanded] = useState(false);
   const [showNikeButton, setShowNikeButton] = useState(false);
   const [showAdidasButton, setShowAdidasButton] = useState(false);
-  const [isBackgroundBlurred, setIsBackgroundBlurred] = useState(false);
+  const [isBackgroundBlurred, setIsBackgroundBlurred] = useState(false); // New state to control background blur
   const [progress, setProgress] = useState(0);
 
   const handleNikeClick = () => {
@@ -30,95 +31,102 @@ function Hero() {
     setIsBackgroundBlurred(true); // Toggle background blur
   };
 
-  const handleLoaderClick = () => {
-    // Reset progress to 0 when the loading bar is clicked
-    setProgress(0);
-  };
-
   return (
-    <div className="relative h-screen overflow-hidden">
-      <LoadingBar
-        color="white"
-        style={{ height: "3px", borderRadius: "1rem" }}
-        progress={progress}
-        onLoaderClick={handleLoaderClick}
-        onLoaderFinished={() => setProgress(0)}
-      />
-
-      <h1
-        className={`absolute top-0 left-0 right-0 text-2xl font-bold font-Monsterrat text-center text-white z-10 mt-8 transition duration-1000 ease-in-out ${
-          isHovered ? "blur-sm" : ""
-        }`}
-      >
-        CHOOSE YOUR ULTIMATE FOOTBALL BOOTS
-      </h1>
-
-      <div className="flex h-full overflow-hidden">
-        <div
-          className={`flex justify-center items-center w-1/2 h-full bg-cover bg-center transition duration-1000 ease-in-out ${
-            isNikeExpanded ? "w-full" : ""
+    <>
+      <div className="relative h-screen overflow-hidden">
+        <LoadingBar
+          color="white"
+          style={{ height: "3px" }}
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+        />
+        <div className="black_overlay"></div>
+        <div className="black_overlay_top2bottom"></div>
+        <h1
+          className={`absolute top-0 left-0 right-0 text-2xl font-bold font-Monsterrat text-center text-white z-10 mt-8 transition duration-1000 ease-in-out ${
+            isHovered ? "blur-sm" : ""
           }`}
-          style={{
-            backgroundImage: `url(${nikeBGimage})`,
-            transform: isHovered ? "scale(1.1)" : "scale(1)",
-            filter: isBackgroundBlurred ? "blur(5px)" : "none", // Apply blur effect only to background
-          }}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
         >
-          <img
-            src={nike}
-            className={`nike_logo size-40 cursor-pointer absolute bg-transparent transition duration-1000 ease-in-out ${
-              isHovered ? "hover:nike-hover" : ""
-            }`}
-            onClick={handleNikeClick}
-            alt=""
-            style={{ zIndex: 1 }}
-          />
-          {showNikeButton && (
-            <button
-              className="absolute z-10 bottom-1/4 bg-white bg-opacity-25 hover:bg-opacity-50 text-white font-semibold py-2 px-4 rounded-md border shadow-md backdrop-blur-lg"
-              onClick={() => setProgress(100)}
-            >
-              Shop NIKE
-            </button>
-          )}
-        </div>
-        <div className="h-full w-0.5 bg-black"></div>
-        <div
-  className={`flex justify-center items-center w-1/2 h-full bg-cover bg-center transition duration-1000 ease-in-out ${
-    isAdidasExpanded ? "w-full" : ""
-  }`}
-  style={{
-    backgroundImage: `url(${adidasBGimage})`,
-    transform: `scale(${isHovered ? 1.1 : 1})`, // Apply scaling to content only, not the background image
-    filter: isBackgroundBlurred ? "blur(5px)" : "none", // Apply blur effect only to background
-  }}
-  onMouseEnter={() => setIsHovered(true)}
-  onMouseLeave={() => setIsHovered(false)}
->
+          CHOOSE YOUR ULTIMATE FOOTBALL BRAND
+        </h1>
 
-          <img
-            src={adidas}
-            className={`adidas_logo size-36 cursor-pointer absolute bg-transparent transition duration-1000 ease-in-out ${
-              isHovered ? "hover:adidas-hover" : ""
-            }`}
+        <div className="flex w-full h-full">
+          <div
+            className={`w-${
+              isNikeExpanded ? "full" : "1/2"
+            } flex justify-center items-center relative overflow-hidden transition-width duration-500`}
+            onClick={handleNikeClick}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <div
+              className={`bg-cover bg-center w-full h-full transition duration-1000 ease-in-out ${
+                isBackgroundBlurred ? "blur-md" : ""
+              }`}
+              style={{
+                backgroundImage: `url(${nikeBgImage})`,
+                filter: isHovered ? "blur(3px)" : "none",
+                transform: isHovered ? "scale(1.1)" : "scale(1)",
+              }}
+            ></div>
+            <img
+              src={nike}
+              className={`nike_logo size-40 cursor-pointer absolute bg-transparent transition duration-1000 ease-in-out ${
+                isHovered ? "hover:nike-hover" : ""
+              }`}
+              alt=""
+              style={{ zIndex: 1 }}
+            />
+            {showNikeButton && (
+              <button
+                className="absolute z-10 bottom-1/4 bg-white bg-opacity-25 hover:bg-opacity-50 text-white font-semibold py-2 px-4 rounded-md border shadow-md backdrop-blur-lg"
+                onClick={() => setProgress(100)}
+              >
+                Shop NIKE
+              </button>
+            )}
+          </div>
+          <div className="h-full w-0.5 bg-black"></div>
+          <div
+            className={`w-${
+              isAdidasExpanded ? "full" : "1/2"
+            } flex justify-center items-center relative overflow-hidden transition duration-500`}
             onClick={handleAdidasClick}
-            alt=""
-            style={{ zIndex: 1 }}
-          />
-          {showAdidasButton && (
-            <button
-              className="absolute z-10 bottom-1/4 bg-white bg-opacity-25 hover:bg-opacity-50 text-white font-semibold py-2 px-4 rounded-md border shadow-md backdrop-blur-lg"
-              onClick={() => setProgress(100)}
-            >
-              Shop ADIDAS
-            </button>
-          )}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <div
+              className={`bg-cover bg-center w-full h-full transition duration-1000 ease-in-out ${
+                isBackgroundBlurred ? "blur-md" : "" // Apply background blur conditionally
+              }`}
+              style={{
+                backgroundImage: `url(${adidasBgImage})`,
+                filter: isHovered ? "blur(3px)" : "none",
+                transform: isHovered ? "scale(1.1)" : "scale(1)",
+              }}
+            ></div>
+            <img
+              src={adidas}
+              className={`adidas_logo size-36 cursor-pointer absolute bg-transparent transition duration-1000 ease-in-out ${
+                isHovered ? "hover:adidas-hover" : ""
+              }`}
+              alt=""
+              style={{ zIndex: 1 }}
+            />
+            {showAdidasButton && (
+              <button
+                className="absolute z-10 bottom-1/4 bg-white bg-opacity-25 hover:bg-opacity-50 text-white font-semibold py-2 px-4 rounded-md border shadow-md backdrop-blur-lg"
+                onClick={() => setProgress(100)}
+              >
+                Shop ADIDAS
+              </button>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
 export default Hero;
+
