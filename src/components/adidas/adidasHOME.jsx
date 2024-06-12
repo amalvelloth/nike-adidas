774c4d73d08c4251c6749365679dea0add15282a
amalvelloth <amalv414@gmail.com>
2024-06-12 23:06:25 +0530

    products page added

--- src/components/adidas/adidasHOME.jsx
+++ src/components/adidas/adidasHOME.jsx
@@ -113,7 +113,7 @@ function AdidasHOME() {
               <img src={adidasimg1} alt="Adidas 1" className="w-auto h-auto object-contain p-2 bg-black" />
               <img src={adidasimg2} alt="Adidas 2" className="w-auto h-auto object-contain p-2 bg-black" />
               <div className="flex w-1/2 sm:w-full h-full items-start justify-center flex-col z-20 p-4">
-                <h1 className="font-bold font-Fira-sans italic  text-2xl  bg-white py-2 sm:fixed sm:top-56 sm:text-center sm:-ml-10 sm:writing-mode-vertical-rl">
+                <h1 className="font-bold font-Fira-sans italic  text-2xl bg-white py-2 sm:fixed sm:top-56 sm:text-center sm:-ml-10 sm:writing-mode-vertical-rl">
                   ADIDAS X SPEEDPORTAL
                 </h1>
               </div>
---
+++ src/components/adidas/adidasPRODUCTS.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function adidasPRODUCTS() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default adidasPRODUCTS
