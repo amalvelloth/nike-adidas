831c8c9e4ee467f2c685c385a638c44f43ca0d84
amalvelloth <amalv414@gmail.com>
2024-06-24 18:44:53 +0530

    added animation on model names by using framer motion

--- src/components/nike/nikeHOME.jsx
+++ src/components/nike/nikeHOME.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ShoppingBagTwoToneIcon from '@mui/icons-material/ShoppingBagTwoTone';
 import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
 import Grid from './nike asssets/grid.svg'
-import { motion, useScroll } from 'framer-motion';
+import { motion, useScroll, useAnimation, useInView } from 'framer-motion';
 
 function NikeHOME() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -20,17 +20,33 @@ function NikeHOME() {
     visible: { x: 0 },
   };
 
+  const controls = useAnimation();
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: false });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    } else {
+      controls.start('hidden');
+    }
+  }, [controls, inView]);
+
+  const animationVariants = {
+    hidden: { x: -100, opacity: 0 },
+    visible: { x: 0, opacity: 1, transition: { duration: 0.5, ease: "easeInOut" } },
+  };
+
   return (
     <>
-      {/* Navbar */}
       {/* Navbar */}
       <nav className="bg-white w-full fixed shadow-lg z-20">
         <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
           {/* Logo */}
           <div>
             <a href="">
-              <svg aria-hidden="true" class="swoosh-svg" focusable="false" viewBox="3 8 18 7" role="img" width="44px" height="44px" fill="none">
-                <path fill="currentColor" fill-rule="evenodd" d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z" clip-rule="evenodd" />
+              <svg aria-hidden="true" className="swoosh-svg" focusable="false" viewBox="3 8 18 7" role="img" width="44px" height="44px" fill="none">
+                <path fill="currentColor" fillRule="evenodd" d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z" clipRule="evenodd" />
               </svg>
             </a>
           </div>
@@ -69,10 +85,6 @@ function NikeHOME() {
 
             </button>
           </div>
-
-
-
-
         </div>
       </nav>
 
@@ -82,7 +94,6 @@ function NikeHOME() {
           className="hidden md:flex fixed inset-0 bg-gray-900 bg-opacity-75 z-50"
           onClick={closeMobileMenu}
         >
-
           <motion.div
             className="absolute top-0 right-0 bottom-0 bg-white w-64 shadow-lg"
             variants={slideInVariants}
@@ -98,7 +109,6 @@ function NikeHOME() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
               </svg>
-
             </button>
             <div className="px-4 py-6 flex-col items-center">
               <a
@@ -141,11 +151,10 @@ function NikeHOME() {
       <section>
         <div className="bg-white overflow-hidden min-h-screen flex items-center justify-center px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 pt-11">
           <div className="relative w-full max-w-lg">
-            <div className="absolute top-0 sm:top-5 -left-40 w-80 h-80 bg-purple-300 rounded-full mix-blend-multiply filter blur-2xl     opacity-70 animate-blob animation-delay-1000"></div>{/* animate-blob */}
-            <div className="absolute top-10 sm:top-4 -right-96 w-80 h-80 bg-yellow-200 rounded-full mix-blend-multiply filter blur-2xl     opacity-70 animate-blob animation-delay-4000"></div>{/* animate-blob animation-delay-2000 */}
-            <div className="absolute -bottom-8 -right-80 w-80 h-80 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl     opacity-70 animate-blob animation-delay-4000"></div>{/* animate-blob animation-delay-4000 */}
-            <div className="absolute -bottom-8 -left-80 sm:-left-60 w-80 h-80 bg-green-300 rounded-full mix-blend-multiply filter blur-2xl     opacity-70 animate-blob animation-delay-2000"></div>{/* animate-blob animation-delay-4000 */}
-
+            <div className="absolute top-0 sm:top-5 -left-40 w-80 h-80 bg-purple-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-1000"></div>{/* animate-blob */}
+            <div className="absolute top-10 sm:top-4 -right-96 w-80 h-80 bg-yellow-200 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-4000"></div>{/* animate-blob animation-delay-2000 */}
+            <div className="absolute -bottom-8 -right-80 w-80 h-80 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-4000"></div>{/* animate-blob animation-delay-4000 */}
+            <div className="absolute -bottom-8 -left-80 sm:-left-60 w-80 h-80 bg-green-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-2000"></div>{/* animate-blob animation-delay-4000 */}
           </div>
           <div className="absolute top-15 right-20 sm:hidden w-80 h-80 bg-blue-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-2000"></div>{/* animate-blob animation-delay-4000 */}
           <div className="absolute bg-[url('/images/nike-mds-speed-8.jpg')] bg-center bg-cover size-5/6 rounded-xl shadow-xl"></div>
@@ -158,20 +167,34 @@ function NikeHOME() {
           <img src={Grid} alt="" className="absolute inset-0 w-full h-full object-cover" />
           <div className="absolute inset-10 sm:inset-1 flex flex-row sm:flex-col md:flex-col justify-between items-start md:items-center text-center">
             {/* Left side with brand names */}
-            <div className=" flex flex-col sm:mt-12 justify-start items-start">
-              <h1 className="text-5xl hover:text-gray-300 transition ease-in-out cursor-pointer font-Lato">MERCURIAL</h1>
+            <div className="flex flex-col sm:mt-12 justify-start items-start" ref={ref}>
+              
+              <motion.h1
+                initial="hidden"
+                animate={controls}
+                variants={animationVariants}
+                className="text-5xl hover:text-gray-300 transition ease-in-out cursor-pointer font-Lato"
+              >MERCURIAL</motion.h1>
               <div className="h-0.5 w-1/2 bg-black"></div>
-              <h1 className="text-5xl hover:text-gray-300 transition ease-in-out cursor-pointer font-Lato">PHANTOM</h1>
+              <motion.h1
+                initial="hidden"
+                animate={controls}
+                variants={animationVariants}
+                className="text-5xl hover:text-gray-300 transition ease-in-out cursor-pointer font-Lato"
+              >PHANTOM</motion.h1>
               <div className="h-0.5 w-1/2 bg-black"></div>
-              <h1 className="text-5xl hover:text-gray-300 transition ease-in-out cursor-pointer font-Lato">TIEMPO</h1>
+              <motion.h1
+                initial="hidden"
+                animate={controls}
+                variants={animationVariants}
+                className="text-5xl hover:text-gray-300 transition ease-in-out cursor-pointer font-Lato"
+              >TIEMPO</motion.h1>
               <div className="h-0.5 w-1/2 bg-black"></div>
 
               <motion.div className="relative flex justify-center items-center shadow-md rounded-t-4xl rounded-b-md my-10 px-4 py-10 w-96 h-80 sm:w-80 sm:h-80 md:w-96 md:h-80 lg:w-96 lg:h-80 xl:w-96 xl:h-80 bg-black">
-                <h1 className="absolute text-white text-2xl font-Monsterrat font-semibold bottom-10"> Gear up for Greatness</h1>
+                <h1 className="absolute text-white text-2xl font-Monsterrat font-semibold bottom-4"> Gear up for Greatness</h1>
               </motion.div>
             </div>
-
-
 
             {/* Right side for additional content */}
             <div className="flex flex-col justify-center items-center sm:mr-2 md:mr-4 mr-10">
@@ -181,7 +204,6 @@ function NikeHOME() {
           </div>
         </div>
       </section>
-
     </>
   );
 }
